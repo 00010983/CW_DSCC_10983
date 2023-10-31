d71a41708c50d56e1467e280848f05039a95d6dc
00010983 <00010983wiut@gmail.com>
2023-10-31 19:16:45 +0500

    Cretion of MVC

---
+++ CW_DSCC_10983_MVC/CW_DSCC_10983_MVC/Controllers/TicketController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW_DSCC_10983_MVC.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: TicketController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TicketController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TicketController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
